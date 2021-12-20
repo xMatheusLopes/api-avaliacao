638e8cdb6666b69cc68970bf93678ed3c9c96b73
Matheus Lopes <mhenrilo@emeal.nttdata.com>
2021-12-20 11:00:25 -0300

    Adicionado cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 module.exports = function (app) {
   const port = 3000;
 
+  var cors = require("cors");
+
   app.listen(port, () => {
     console.log(`App rodando na porta ${port}`);
   });
+
+  app.use(cors());
 };
